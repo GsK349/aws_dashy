bcddda8b4f0cf42a98e40197c1cec55ec89bbd85
bizGsK349 <sai.guguloth@bizacuity.com>
2023-08-29 22:34:23 +0530

    list s3 buckets

--- main.go
+++ main.go
@@ -4,20 +4,34 @@ import (
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"os"
-)
-
-var (
-	bucketname      string
-	objectPrefix    string
-	objectDelimiter string
-	maxKeys         int
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 func main() {
-	sess, err := session.NewSession(&aws.Config{})
+	sess, err := session.NewSession(
+		&aws.Config{
+			Region: aws.String("ap-south-1"),
+		},
+	)
 	if err != nil {
 		fmt.Println("Error creating a session: %v", err)
 		return
 	}
+
+	//Creating a s3 client
+	s3Client := s3.New(sess)
+
+	//getting all the buckets in the account
+	results, err := s3Client.ListBuckets(nil)
+	if err != nil {
+		fmt.Println("Error Listing Buckets: %v", err)
+		return
+	}
+
+	//Listing buckets
+	fmt.Println("S3 buckets:")
+	for _, bucket := range results.Buckets {
+		fmt.Println(*bucket.Name)
+	}
+
 }
