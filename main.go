7dcf25f6f7fa94a6c0906a42cb5eda747858e2d6
bizGsK349 <sai.guguloth@bizacuity.com>
2023-08-29 21:57:33 +0530

    Created a AWS connection
    1. created AWS connection 2.setup AWS environment in github runner 3.updated readme with appropriate steps

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"os"
 )
 
-func mainMain() {
-	fmt.Println("Hello world")
-}
+var (
+	bucketname      string
+	objectPrefix    string
+	objectDelimiter string
+	maxKeys         int
+)
+
 func main() {
-	mainMain()
+	sess, err := session.NewSession(&aws.Config{})
+	if err != nil {
+		fmt.Println("Error creating a session: %v", err)
+		return
+	}
 }
